cd854f53ee7a2c01d9972956c22d42016f6ec972
dantheman8300 <daniel.7039@hotmail.com>
2023-05-02 18:36:39 -0700

    added backend

---
+++ studio-background/src/compile.ts
@@ -0,0 +1,236 @@
+import { JsonRpcProvider, Ed25519Keypair, RawSigner } from '@mysten/sui.js';
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import { Project } from './schema/user-schema';
+import dotenv from 'dotenv';
+// import stripAnsi from 'strip-ansi';
+
+const TEMP_DIR = `${__dirname}/../temp-packages`;
+
+// const exampleModule = fs.readFileSync(`${__dirname}/example.move`, 'utf8');
+
+// const exampleProject = {
+//   package: 'test',
+//   modules: [
+//     {
+//       name: 'test',
+//       code: exampleModule,
+//     },
+//   ],
+//   dependencies: [
+//     {
+//       name: 'Sui',
+//       address: '0x2',
+//     },
+//   ],
+// };
+
+type TestReturn = {
+  result: string;
+  errorCode: string;
+  error: boolean;
+}
+
+export async function compile(project: Project): Promise<string | string[]> {
+
+  // Created temporary project in user directory
+  const tempProjectPath = `${TEMP_DIR}/${project.package}`;
+
+  // console.log(tempProjectPath)
+
+  // Create the project directory
+  fs.mkdirSync(tempProjectPath, { recursive: true });
+
+  // Created the project's sources directory
+  const tempProjectSourcesPath = `${tempProjectPath}/sources`;
+  fs.mkdirSync(tempProjectSourcesPath, { recursive: true });
+
+  // Add the module files to the project's sources directory
+  project.modules.forEach((module) => {
+    fs.writeFileSync(`${tempProjectSourcesPath}/${module.name}.move`, module.code);
+  });
+
+  // Create toml file based on the project's dependencies and project name
+  let addresses = '';
+  project.dependencies.forEach((dependency) => {
+    addresses += `${dependency.name} = "${dependency.address}"\n`;
+  });
+
+  // NOTE: remove line in addresses once it is a default dependency in the FE
+  const toml = `
+    [package]
+    name = "${project.package}"
+    version = "0.0.1"
+
+    [dependencies]
+    Sui = { git = "https://github.com/MystenLabs/sui.git", subdir = "crates/sui-framework/packages/sui-framework", rev = "testnet" }
+
+    [addresses]
+    ${addresses}
+  `;
+
+  fs.writeFileSync(
+    `${tempProjectPath}/Move.toml`,
+    toml
+  )
+
+  // Compile the project
+  try {
+    const compiledModules = execSync(
+      `sui move build --dump-bytecode-as-base64 --path ${tempProjectPath}`,
+      { encoding: 'utf-8'}
+    );
+
+    console.log("compiledModules", compiledModules);
+
+    // Remove the temporary project directory
+    fs.rmdirSync(tempProjectPath, { recursive: true });
+
+
+    return compiledModules as unknown as string[];
+
+  } catch (error: any) {
+    console.log('error', error)
+    const errorMessage = error.stdout;
+
+    // Check error message for update needed message - TODO
+
+    // Remove the temporary project directory
+    fs.rmdirSync(tempProjectPath, { recursive: true });
+    
+
+    return errorMessage as string;
+  }
+}
+
+export async function test(project: Project): Promise<TestReturn> {
+
+  // Created temporary project in user directory
+  const tempProjectPath = `${TEMP_DIR}/${project.package}`;
+
+  // console.log(tempProjectPath)
+
+  // Create the project directory
+  fs.mkdirSync(tempProjectPath, { recursive: true });
+
+  // Created the project's sources directory
+  const tempProjectSourcesPath = `${tempProjectPath}/sources`;
+  fs.mkdirSync(tempProjectSourcesPath, { recursive: true });
+
+  // Add the module files to the project's sources directory
+  project.modules.forEach((module) => {
+    fs.writeFileSync(`${tempProjectSourcesPath}/${module.name}.move`, module.code);
+  });
+
+  // Create toml file based on the project's dependencies and project name
+  let addresses = '';
+  project.dependencies.forEach((dependency) => {
+    addresses += `${dependency.name} = "${dependency.address}"\n`;
+  });
+
+  // NOTE: remove line in addresses once it is a default dependency in the FE
+  const toml = `
+    [package]
+    name = "${project.package}"
+    version = "0.0.1"
+
+    [dependencies]
+    Sui = { git = "https://github.com/MystenLabs/sui.git", subdir = "crates/sui-framework/packages/sui-framework", rev = "testnet" }
+
+    [addresses]
+    ${addresses}
+  `;
+
+  fs.writeFileSync(
+    `${tempProjectPath}/Move.toml`,
+    toml
+  )
+
+  // Compile the project
+  try {
+    const test = execSync(
+      `sui move test --path ${tempProjectPath}`,
+      { encoding: 'utf-8'}
+    );
+
+    // console.log("test", test);
+
+    // Find the index of the unit test results
+    const testResultsIndex = test.search("Running Move unit tests");
+
+    // Get the unit test results
+    const testResults = test.slice(testResultsIndex);
+
+    console.log("testResults", testResults);
+
+    // console.log("compiledModules", compiledModules);
+
+    // Remove the temporary project directory
+    fs.rmdirSync(tempProjectPath, { recursive: true });
+
+
+    return {
+      result: testResults,
+      errorCode: "",
+      error: false
+    }
+
+  } catch (error: any) {
+    console.log('error', error)
+    const errorMessageToIgnore = error.stdout;
+    const errorMessage = error.stderr.replace(errorMessageToIgnore, '');
+
+    // Find the index of the unit test results
+    const testResultsIndex = errorMessageToIgnore.search("Running Move unit tests");
+
+    // Get the unit test results
+    const testResults = errorMessageToIgnore.slice(testResultsIndex);
+
+
+    console.log("errorMessage", errorMessage)
+    console.log("testResults", testResults)
+
+
+    // let testResultsIndex = errorMessage.search("error");
+    // if (testResultsIndex === -1) {
+    //   testResultsIndex = errorMessage.search("warning");
+    // }
+
+    // // Get the unit test results
+    // const testResults = errorMessage.slice(testResultsIndex)
+    
+    // console.log("testResults", testResults)
+
+    // Check error message for update needed message - TODO
+
+    // Remove the temporary project directory
+    fs.rmdirSync(tempProjectPath, { recursive: true });
+
+    // if (errorMessage.includes("Running Move unit tests")) {
+    //   return {
+    //     result: "",
+    //     errorCode: errorMessage,
+    //     error: 1
+    //   };
+    // }
+    
+
+    if (errorMessageToIgnore.includes("Failed to build Move modules: Compilation error.")) {
+      return {
+        result: testResults,
+        errorCode: errorMessage,
+        error: true
+      };
+    }
+
+    return {
+      result: testResults,
+      errorCode: errorMessage,
+      error: false
+    };
+
+  }
+}
+
+
+
---
+++ studio-background/src/index.ts
@@ -0,0 +1,101 @@
+import cors from 'cors';
+import express from 'express';
+import { compile, test } from './compile';
+// import { getObjectDetails, getPackageDetails } from './object-details';
+
+const app = express();
+const portHttp = 80;
+const portHttps = 443;
+
+// // PRODUCTION
+// import https from 'https';
+// import fs from 'fs';
+// const CERT_PATH = "/etc/letsencrypt/live/api.movestudio.dev/fullchain.pem"
+// const KEY_PATH = "/etc/letsencrypt/live/api.movestudio.dev/privkey.pem"
+// const options = {
+//   key: fs.readFileSync(KEY_PATH),
+//   cert: fs.readFileSync(CERT_PATH)
+// };
+// const httpsServer = https.createServer(options, app);
+// httpsServer.listen(portHttps, () => {
+// 	console.log('HTTPs Server running on port ', portHttps);
+// });
+
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/projects', (req, res) => {
+  res.send('projects');
+});
+
+app.post('/compile', async (req, res) => {
+  const project = req.body;
+
+  // console.log(project);
+  console.log('compiling project...')
+
+  // Call compile function
+  const compileResult = await compile(project);
+
+  // console.log(compileResult)
+
+  res.send(compileResult);
+
+});
+
+app.post('/test', async (req, res) => {
+  const project = req.body;
+
+  // console.log(project);
+  console.log('testing project...')
+
+  // Call compile function
+  const testResults = await test(project);
+
+  // console.log(compileResult)
+
+  res.send(testResults);
+
+});
+
+// app.post('/object-details', async (req, res) => {
+//   const objectId = req.body.objectId as string;
+//   const rpc = req.body.rpc as string;
+
+//   console.log('Retrieving object details for: ' + objectId)
+
+//   // console.log(objectId);
+
+//   const objectDetails = await getObjectDetails(objectId, rpc);
+
+//   // console.log(objectDetails);
+
+//   res.send(objectDetails);
+// });
+
+// app.post('/package-details', async (req, res) => {
+//   const packageId = req.body.packageId as string;
+//   const rpc = req.body.rpc as string;
+
+//   // console.log(packageId);
+//   console.log('Retrieving package details for: ' + packageId)
+
+//   const packageDetails = await getPackageDetails(packageId, rpc);
+
+//   // console.log(packageDetails);
+
+//   res.send(packageDetails);
+// });
+
+app.listen(process.env.PORT || portHttp, () => {
+  console.log(`REST API is listening on port: ${process.env.PORT || portHttp}.`);
+});
