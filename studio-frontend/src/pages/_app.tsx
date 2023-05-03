bfcb90aeb905915a304b96e0d9e0e699bf80d3e8
dantheman8300 <daniel.7039@hotmail.com>
2023-05-03 14:58:25 -0700

    added analytics

--- studio-frontend/src/pages/_app.tsx
+++ studio-frontend/src/pages/_app.tsx
@@ -4,10 +4,13 @@ import type { AppProps } from 'next/app'
 import {WalletProvider} from '@suiet/wallet-kit';
 import '@suiet/wallet-kit/style.css';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WalletProvider>
       <Component {...pageProps} />
+      <Analytics/>
     </WalletProvider>
   )
 }
